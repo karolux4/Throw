73a737f7ef2ad2f1c4c79506b6069112eaba22af
karolux4 <karolisbutkus4@gmail.com>
2019-02-21 12:31:08 +0200

    a

---
+++ Throw Experiment/Assets/SteamVR/Extras/OnCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 4)
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            if (gameObject.transform.localPosition.x > 1f)
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"D:\VR Lab\Experiment_data.txt", true))
+                {
+                    file.WriteLine(gameObject.transform.localPosition.x + " " + gameObject.transform.localPosition.y + " " + gameObject.transform.localPosition.z);
+                }
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Throw Experiment/Assets/SteamVR/Extras/SteamVR_TestThrow.cs
@@ -0,0 +1,108 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.Extras
+{
+    [RequireComponent(typeof(SteamVR_TrackedObject))]
+    public class SteamVR_TestThrow : MonoBehaviour
+    {
+        public GameObject prefab;
+        public Rigidbody attachPoint;
+        private bool allowed_to_throw;
+        private int delta = 0;
+        private float time = 0;
+        private float velocity = 0;
+        private Vector3 max_velocity;
+        private Vector3 max_angular_velocity;
+
+        public SteamVR_Action_Boolean spawn = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+
+        SteamVR_Behaviour_Pose trackedObj;
+        FixedJoint joint;
+
+        private void Awake()
+        {
+            trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
+        }
+
+        private void FixedUpdate()
+        {
+            Debug.Log(trackedObj.origin.TransformVector(trackedObj.GetVelocity())+" "+ trackedObj.origin.TransformVector(trackedObj.GetVelocity()).magnitude);
+            if (joint == null)
+            {
+                GameObject go = GameObject.Instantiate(prefab);
+                go.transform.position = attachPoint.transform.position;
+
+                joint = go.AddComponent<FixedJoint>();
+                joint.connectedBody = attachPoint;
+            }
+            else if (joint != null && allowed_to_throw && time>0.3f)
+            {
+                GameObject go = joint.gameObject;
+                Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+                Object.DestroyImmediate(joint);
+                joint = null;
+
+                // We should probably apply the offset between trackedObj.transform.position
+                // and device.transform.pos to insert into the physics sim at the correct
+                // location, however, we would then want to predict ahead the visual representation
+                // by the same amount we are predicting our render poses.
+
+                Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+                if (origin != null)
+                {
+                    rigidbody.velocity = max_velocity;
+                    rigidbody.angularVelocity = max_angular_velocity;
+                }
+                else
+                {
+                    rigidbody.velocity = max_velocity;
+                    rigidbody.angularVelocity = max_angular_velocity;
+                }
+
+                rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+                allowed_to_throw = false;
+                delta = 0;
+                velocity = 0;
+                max_velocity = new Vector3(0,0,0);
+                max_angular_velocity = new Vector3(0, 0, 0);
+                time = 0;
+                throw new System.Exception("Throw");
+            }
+            else
+            {
+                if (time > 0.75f)
+                {
+                    Check_Throw(ref velocity, ref delta,ref max_velocity, ref max_angular_velocity);
+                }
+                else
+                {
+                    time += Time.deltaTime;
+                }
+            }
+        }
+        private void Check_Throw(ref float prev_velocity, ref int delta, ref Vector3 max_velocity,ref Vector3 max_angular_velocity)
+        {
+            Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+            if (prev_velocity > 2f && prev_velocity > origin.TransformVector(trackedObj.GetVelocity()).magnitude)
+            {
+                if (delta > 0)
+                {
+                    allowed_to_throw = true;
+                }
+                else
+                {
+                    delta++;
+                }
+            }
+            else
+            {
+                prev_velocity = origin.TransformVector(trackedObj.GetVelocity()).magnitude;
+                max_velocity = origin.TransformVector(trackedObj.GetVelocity());
+                max_angular_velocity = origin.TransformVector(trackedObj.GetAngularVelocity());
+                allowed_to_throw = false;
+            }
+        }
+    }
+}
